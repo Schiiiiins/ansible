8c5657a22d548e249e34f98e7b61140b8edd3442
marc <marc_schneider@gmx.ch>
2019-04-23 14:35:59 +0200

    added dynInventory-folder

---
+++ dynInventory/inventory.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import json
+
+# Not really dynamic
+def get_inventory_data():
+    return {
+        "databases": {
+            "hosts": ["db_server"],
+            "vars": {
+                "ansible_ssh_pass": "Passw0rd",
+                "ansible_ssh_host": "192.168.1.1"
+                }
+        },
+        "web": {
+            "hosts": ["web_server"],
+            "vars": {
+                "ansible_ssh_pass": "Passw0rd",
+                "ansible_ssh_host": "192.168.1.2"
+            }
+        }
+    }
+
+def read_cli_args():
+    global args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--list', action='store_true')
+    parser.add_argument('--host', action='store')
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    global args
+    read_cli_args()
+    inventory_data = get_inventory_data()
+    if args and args.list:
+        print(json.dumps(inventory_data))
+    elif args.host:
+        print(json.dumps({'meta': {'hostvars': {}}}))
